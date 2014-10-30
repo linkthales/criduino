801f49706f0e9fefc7874254036bdbea9da2f964
Gabriel Carreira Lovetro <gabe_man2001@yahoo.com.br>
2014-10-30 10:14:33 -0200

    Ajustes gerais. Implementação de locales.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Criduino::Application.routes.draw do
-  resources :users
+  scope ":locale", :locale => /en|pt\-BR/ do
+    resources :users
+	
+	resource :user_confirmation, :only => [:show]
+  end
   
   root :to => "home#index"
 end
